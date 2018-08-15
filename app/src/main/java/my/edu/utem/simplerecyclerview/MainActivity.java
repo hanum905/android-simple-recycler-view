61490c4268215b6a68357fbf339d78db0a1a8f0b
Hanum <hanum905@gmail.com>
2018-08-15 16:57:14 +0800

    Simple RecylerView

---
+++ app/src/main/java/my/edu/utem/simplerecyclerview/MainActivity.java
@@ -0,0 +1,69 @@
+package my.edu.utem.simplerecyclerview;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        CustomAdapter adapter = new CustomAdapter(getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder{
+
+        TextView wordTextView;
+
+        public CustomViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.custom_row, parent, false));
+            wordTextView = itemView.findViewById(R.id.wordTextView);
+        }
+    }
+
+    //find new id by row, adapter utk link data dkt UI
+    public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder>{
+
+
+        public final String[] restaurantNames;
+        public CustomAdapter(Context context){
+            Resources resources = context.getResources();
+            restaurantNames = resources.getStringArray(R.array.restaurant_name);
+
+        }
+
+        @NonNull
+        @Override
+       //customviewholder akan link custom row
+        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new CustomViewHolder(LayoutInflater.from(viewGroup.getContext()), viewGroup);
+        }
+
+        @Override
+        //apa nk tunjuk
+        public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int i) {
+            customViewHolder.wordTextView.setText(restaurantNames[i]);
+
+        }
+
+        @Override
+        //berapa kali nk loop
+        public int getItemCount() {
+            return restaurantNames.length;
+        }
+    }
+}
